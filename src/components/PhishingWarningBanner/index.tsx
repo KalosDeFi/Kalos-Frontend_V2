e8bf0dda12c1316d036289f210c070262815d738
Kalos Protocol <kaizendex@protonmail.com>
2023-01-02 23:30:36 +1100

    Update index.tsx

--- src/components/PhishingWarningBanner/index.tsx
+++ src/components/PhishingWarningBanner/index.tsx
@@ -79,11 +79,7 @@ const PhishingWarningBanner: React.FC = () => {
       ) : (
         <>
           <InnerContainer>
-            <picture>
-              <source type="image/webp" srcSet="/images/decorations/phishing-warning-bunny.webp" />
-              <source type="image/png" srcSet="/images/decorations/phishing-warning-bunny.png" />
-              <img src="/images/decorations/phishing-warning-bunny.png" alt="phishing-warning" width="92px" />
-            </picture>
+            
             <SpeechBubble>{warningTextComponent}</SpeechBubble>
           </InnerContainer>
           <IconButton onClick={hideBanner} variant="text">
