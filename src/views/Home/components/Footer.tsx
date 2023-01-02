951acba3740cf007c7ad73a8bcbedac7c1b503d7
Kalos Protocol <kaizendex@protonmail.com>
2023-01-03 01:27:30 +1100

    Update Footer.tsx

--- src/views/Home/components/Footer.tsx
+++ src/views/Home/components/Footer.tsx
@@ -1,5 +1,5 @@
 //import styled from 'styled-components'
-import { /* Flex, */ Heading, Text, Link, useMatchBreakpointsContext } from '@pancakeswap/uikit'
+import { /* Flex, */ Heading, Text, Link, /* useMatchBreakpointsContext */ } from '@pancakeswap/uikit'
 import { useTranslation } from 'contexts/Localization'
 import ConnectWalletButton from 'components/ConnectWalletButton'
 // import Container from 'components/Layout/Container'
