c924d380c743f6e5032cbe1ec9bdac7db68259d3
Kalos Protocol <kaizendex@protonmail.com>
2023-01-04 12:19:28 +1100

    Update Footer.tsx

--- src/components/Menu/Footer.tsx
+++ src/components/Menu/Footer.tsx
@@ -1,8 +1,8 @@
 import { memo } from 'react'
 import styled from 'styled-components'
-import { /* ButtonMenu, ButtonMenuItem, */ LinkExternal, Flex, Svg, Image, Button } from '@pancakeswap/uikit'
+import { /* ButtonMenu, ButtonMenuItem, */ LinkExternal, Flex, /* Svg, Image, Button */ } from '@pancakeswap/uikit'
 import { useTranslation } from 'contexts/Localization'
-import { EXCHANGE_DOCS_URLS } from 'config/constants'
+// import { EXCHANGE_DOCS_URLS } from 'config/constants'
 
 const Wrapper = memo(styled.div<{ $isSide: boolean }>`
   width: 100%;
@@ -40,7 +40,7 @@ type FooterVariant = 'default' | 'side'
 
 const Footer: React.FC<{ variant?: FooterVariant; helpUrl?: string }> = ({
   variant = 'default',
-  helpUrl = EXCHANGE_DOCS_URLS,
+  // helpUrl = EXCHANGE_DOCS_URLS,
 }) => {
   const { t } = useTranslation()
   const isSide = variant === 'side'
@@ -65,15 +65,7 @@ const Footer: React.FC<{ variant?: FooterVariant; helpUrl?: string }> = ({
         width={['100%', '100%', '100%', isSide ? '100%' : 'auto']}
         justifyContent={['center', 'center', 'center', 'flex-end']}
       >
-        <BubbleWrapper>
-          <Button id="clickExchangeHelp" as="a" external href={helpUrl} variant="subtle">
-            {t('Need help ?')}
-          </Button>
-          <Svg viewBox="0 0 16 16">
-            <path d="M0 16V0C0 0 3 1 6 1C9 1 16 -2 16 3.5C16 10.5 7.5 16 0 16Z" />
-          </Svg>
-        </BubbleWrapper>
-        <Image src="/images/help.png" alt="Get some help" width={160} height={108} />
+        
       </Flex>
     </Wrapper>
   )
