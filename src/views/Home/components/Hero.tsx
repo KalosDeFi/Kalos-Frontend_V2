6a6acb82d552e8e5a803ae2e1af61579f3272185
Kalos Protocol <kaizendex@protonmail.com>
2023-01-04 15:11:03 +1100

    Update Hero.tsx

--- src/views/Home/components/Hero.tsx
+++ src/views/Home/components/Hero.tsx
@@ -102,7 +102,7 @@ const Hero = () => {
         flexDirection={['column-reverse', null, null, 'row']}
         alignItems={['flex-end', null, null, 'center']}
         justifyContent="center"
-        mt={[account ? '280px' : '50px', null, 0]}
+        mt={[account ? '240px' : '50px', null, 0]}
         id="homepage-hero"
       >
         <Flex flex="1" flexDirection="column">
