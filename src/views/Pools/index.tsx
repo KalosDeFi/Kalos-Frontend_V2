a1c6b1b0767025493249f0b5a1950393edd3d814
Kalos Protocol <kaizendex@protonmail.com>
2023-01-06 02:51:17 +1100

    indx

--- src/views/Pools/index.tsx
+++ src/views/Pools/index.tsx
@@ -263,10 +263,10 @@ const Pools: React.FC = () => {
         <Flex justifyContent="space-between" flexDirection={['column', null, null, 'row']}>
           <Flex flex="1" flexDirection="column" mr={['8px', 0]}>
             <Heading as="h1" scale="xxl" color="secondary" mb="24px">
-              {t('Syrup Pools')}
+              {t('Kalos Pools')}
             </Heading>
             <Heading scale="md" color="text">
-              {t('Just stake some tokens to earn.')}
+              {t('Stake single tokens to earn.')}
             </Heading>
             <Heading scale="md" color="text">
               {t('High APR, low risk.')}
