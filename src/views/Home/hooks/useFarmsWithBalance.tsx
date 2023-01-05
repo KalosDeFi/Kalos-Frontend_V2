e9cc75f2b02e0c50215bb7f8af6ab9c40b77a4d7
Kalos Protocol <kaizendex@protonmail.com>
2023-01-05 23:20:50 +1100

    rev

--- src/views/Home/hooks/useFarmsWithBalance.tsx
+++ src/views/Home/hooks/useFarmsWithBalance.tsx
@@ -28,7 +28,7 @@ const useFarmsWithBalance = () => {
       const farmsCanFetch = farmsConfig.filter((f) => poolLength > f.pid)
       const calls = farmsCanFetch.map((farm) => ({
         address: getMasterChefAddress(),
-        name: 'pendingCake',
+        name: 'pendingXalo',
         params: [farm.pid, account],
       }))
 
