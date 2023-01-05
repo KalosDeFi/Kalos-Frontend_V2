43c8fc0cb5bae0aa67807fc1856d1f8dc9f40c80
Kalos Protocol <kaizendex@protonmail.com>
2023-01-06 03:20:33 +1100

    pool

--- src/views/Pools/components/LockedPool/LockedStakingApy.tsx
+++ src/views/Pools/components/LockedPool/LockedStakingApy.tsx
@@ -69,7 +69,7 @@ const LockedStakingApy: React.FC<LockedStakingApyProps> = ({
       <Flex justifyContent="space-between" mb="16px">
         <Box>
           <Text color="textSubtle" textTransform="uppercase" bold fontSize="12px">
-            {t('CAKE locked')}
+            {t('XALO locked')}
           </Text>
           <BalanceWithLoading color="text" bold fontSize="16px" value={currentLockedAmount} decimals={5} />
           <BalanceWithLoading
@@ -122,7 +122,7 @@ const LockedStakingApy: React.FC<LockedStakingApyProps> = ({
       )}
       <Flex alignItems="center" justifyContent="space-between">
         <Text color="textSubtle" textTransform="uppercase" bold fontSize="12px">
-          {t('Recent CAKE profit')}
+          {t('Recent XALO profit')}
         </Text>
         <BalanceWithLoading color="text" bold fontSize="16px" value={earningTokenBalance} decimals={5} />
       </Flex>
