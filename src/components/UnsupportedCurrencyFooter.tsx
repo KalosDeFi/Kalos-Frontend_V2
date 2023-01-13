2a71eac7db2600e982584f54fd84687bbc623e51
Kalos Protocol <kaizendex@protonmail.com>
2023-01-14 03:26:43 +1100

    sdk

--- src/components/UnsupportedCurrencyFooter.tsx
+++ src/components/UnsupportedCurrencyFooter.tsx
@@ -1,4 +1,4 @@
-import { Currency, Token } from '@pancakeswap/sdk'
+import { Currency, Token } from '@kalosdefi/sdk'
 import { Button, Text, Modal, useModal, InjectedModalProps, Link } from '@pancakeswap/uikit'
 import { useTranslation } from 'contexts/Localization'
 import styled from 'styled-components'
