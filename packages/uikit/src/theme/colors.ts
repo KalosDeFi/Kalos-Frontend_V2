c650f1aceee013fbba1e0074b72108f4d9ce6b8c
Kalos Protocol <kaizendex@protonmail.com>
2023-02-09 01:13:02 +1100

    color

--- packages/uikit/src/theme/colors.ts
+++ packages/uikit/src/theme/colors.ts
@@ -2,7 +2,7 @@ import { Colors } from "./types";
 
 export const baseColors = {
   failure: "#ED4B9E",
-  primary: "#135075",
+  primary: "#067039",
   primaryBright: "#53DEE9",
   primaryDark: "#0098A1",
   secondary: "#473ef7",
